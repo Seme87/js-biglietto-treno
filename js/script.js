20d86825908e1fc3cb0b06f3ae3ec020a2060e1d
Seme87 <massimomonteleone@hotmail.com>
2022-10-20 15:38:21 +0200

    aggiunge controllo in consolle log sulla condizione

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const age = Number( prompt("inserisci la tua età") );
 const chilometers = Number( prompt("inserisci la distanza in km") );
 //PREZZO DEL BIGLIETTO
 let price = (chilometers * 0.21);
-console.log(price);21
+console.log(price);
 
 //PREZZO DEL BIGLIETTO SE L'ETA' DELL'UTENTE E MINORE O UGUALE A 18 ANNI O MAGGIORE UGUALE DI 65
 if( age <= 18 ) {
@@ -12,5 +12,7 @@ if( age <= 18 ) {
    
 } else if( age >= 65 ) {
     price = ( chilometers * 0.21 ) * 0.40 ;
-    console.log( price);
+    console.log( price ) ;
 }
+
+console.log( price ) ;
