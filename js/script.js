d9179b6592054a57b1d33de2b323860b08297603
Seme87 <massimomonteleone@hotmail.com>
2022-10-21 13:21:45 +0200

    fix price

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ const age = Number( prompt("inserisci la tua età") );
 // CHIEDERE ALL'UTENTE LA DISTANZA IN KM'
 const chilometers = Number( prompt("inserisci la distanza in km") );
 //PREZZO DEL BIGLIETTO
-let price = (chilometers * 0.21);
+let price = chilometers * 0.21;
 console.log(price);
 
 //PREZZO DEL BIGLIETTO SE L'ETA' DELL'UTENTE E MINORE O UGUALE A 18 ANNI O MAGGIORE UGUALE DI 65
 if( age <= 18 ) {
-    price = ( chilometers * 0.21 ) * 0.20 ;
+    //price = price- price * 0.2;
+    price -= price * 0.20 ;
    
 } else if( age >= 65 ) {
-    price = ( chilometers * 0.21 ) * 0.40 ;
+    //price = price- price * 0.4;
+    price -= price * 0.40 ;
     console.log( price ) ;
 }
 
